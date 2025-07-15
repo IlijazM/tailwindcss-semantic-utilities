2edbb3f9b4693da128520513ffe506a4b0cc138b
IlijazM <mehmedovic.ilijaz@gmail.com>
2025-07-15 15:30:52 +0200

    words on theme overrides for utility colors

--- tailwindcss-semantic-colors/src/options.ts
+++ tailwindcss-semantic-colors/src/options.ts
@@ -57,8 +57,65 @@ export const DEFAULT_OPTIONS: TailwindCssSemanticColorsOptionsType = {
   themeOverrides: {},
 };
 
+export type COLOR_TYPES = 'semanticColors' | 'surfaceColors' | 'contentColors';
+
+export const SEMANTIC_COLORS_KEY: COLOR_TYPES = 'semanticColors';
+export const SURFACE_COLORS_KEY: COLOR_TYPES = 'surfaceColors';
+export const CONTENT_COLORS_KEY: COLOR_TYPES = 'contentColors';
+
 export class TailwindCssSemanticColorsOptions extends TailwindcssOptionsObject<TailwindCssSemanticColorsOptionsType> {
   constructor(options: any) {
     super(options, DEFAULT_OPTIONS);
+  }
+
+  get themes() {
+    return this.get('themes');
+  }
+
+  get defaultTheme() {
+    return this.get('defaultTheme');
+  }
+
+  get semanticColors() {
+    return this.get(SEMANTIC_COLORS_KEY);
+  }
+
+  get surfaceColors() {
+    return this.get(SURFACE_COLORS_KEY);
+  }
+
+  get contentColors() {
+    return this.get(CONTENT_COLORS_KEY);
+  }
+
+  get themeOverrides() {
+    return this.get('themeOverrides');
+  }
+
+  getThemeOverridesFor(colorTypes: COLOR_TYPES | COLOR_TYPES[]): string[] {
+    let themeOverrides: string[] = [];
+
+    for (const theme of this.themes) {
+      const themeOverride = this.themeOverrides[theme];
+      if (!themeOverride) {
+        continue;
+      }
+
+      if (Array.isArray(colorTypes)) {
+        for (const colorType of colorTypes) {
+          if (colorType in themeOverride) {
+            themeOverrides.push(...Object.keys(themeOverride[colorType]));
+            break;
+          }
+        }
+      } else {
+        if (colorTypes in themeOverride) {
+          themeOverrides.push(...Object.keys(themeOverride[colorTypes]));
+          break;
+        }
+      }
+    }
+
+    return [...new Set(themeOverrides)];
   }
 }
