b21623d75428af4821aba8e1efc1befe5cb0c62a
IlijazM <mehmedovic.ilijaz@gmail.com>
2025-11-14 09:24:04 +0100

    adds tailwindcss plugin to storybook to tailwindcss-semantic-palette

--- tailwindcss-semantic-palette/storybook/src/stories/Palette.tsx
+++ tailwindcss-semantic-palette/storybook/src/stories/Palette.tsx
@@ -1,8 +1,10 @@
+import "./button.css";
+
 /** Primary UI component for user interaction */
 export const Palette = () => {
   return (
     <div>
-      <div className="w-64 h-64 bg-primary">
+      <div className="w-64 h-64 bg-blue-500">
         Foo
       </div>
     </div>
